d203869a0fa73343907f9dd7ccde8369cc5419fc
Nega0619 <116786798+hwihwi619@users.noreply.github.com>
2022-12-19 11:22:26 +0900

    add pdfMaker Files rm Scratch Files
    json 형식에 맞춰서 그래프만 뿌리면 됨

---
+++ pdfMaker/src/js/AllAnalysisGraphs.js
@@ -0,0 +1,132 @@
+var NORMAL = [0, 1];
+var ATTENTION = [1, 2];
+var CAUTION = [2, 3];
+var ALERT = [3, 4];
+var DANGER = [4, 5];
+
+var COLOR_NORMAL = "#64D3B5";
+var COLOR_ATTENTION = "#A7E55F";
+var COLOR_CAUTION = "#FAE63F";
+var COLOR_ALERT = "#FD871F";
+var COLOR_DANGER = "#E85555";
+
+var LIST_DISEASE = ["간암", "위암", "폐암", "대장암", "갑상선암", "유방암"];
+var target_list = ["정상", "관심", "위험", "경계", "주의", "주의"];
+
+function get_data(target, target_pos, target_list) {
+    const result = new Array(LIST_DISEASE.length);
+    for (const [index, element] of target_list.entries()) {
+        // result[index] = element == target ? target_pos : 0;
+        if (element == target) {
+            result[index] = target_pos;
+        } else {
+            result[index] = 0;
+        }
+    }
+
+    return result;
+}
+
+const data = {
+    labels: LIST_DISEASE,
+    datasets: [
+        {
+            label: "정상",
+            data: get_data("정상", NORMAL, target_list),
+            backgroundColor: COLOR_NORMAL,
+        },
+        {
+            label: "관심",
+            data: get_data("관심", ATTENTION, target_list),
+            backgroundColor: COLOR_ATTENTION,
+        },
+        {
+            label: "주의",
+            data: get_data("주의", CAUTION, target_list),
+            backgroundColor: COLOR_CAUTION,
+        },
+        {
+            label: "경계",
+            data: get_data("경계", ALERT, target_list),
+            backgroundColor: COLOR_ALERT,
+        },
+        {
+            label: "위험",
+            data: get_data("위험", DANGER, target_list),
+            backgroundColor: COLOR_DANGER,
+        },
+    ],
+};
+
+const config = {
+    type: "bar",
+    data: data,
+    options: {
+        animation: false,
+        // animation: {
+        //     duration: 0,
+        //     hover: {
+        //         animationDuration: 0, // duration of animations when hovering an item
+        //     },
+        //     responsiveAnimationDuration: 0, // animation duration after a resize
+        // },
+        indexAxis: "y",
+        plugins: {
+            legend: {
+                align: "center",
+                // padding: ,
+                labels: {
+                    font: {
+                        size: 15,
+                    },
+                },
+                ticks: {
+                    font: {
+                        size: 15,
+                    },
+                },
+            },
+        },
+        responsive: false,
+        scales: {
+            x: {
+                stacked: true,
+                ticks: {
+                    callback: function (value) {
+                        if (value == 1) return "정상";
+                        else if (value == 2) return "관심";
+                        else if (value == 3) return "주의";
+                        else if (value == 4) return "경계";
+                        else if (value == 5) return "위험";
+                    },
+                    display: false,
+                },
+            },
+            y: {
+                stacked: true,
+                ticks: {
+                    font: {
+                        size: 15,
+                    },
+                },
+            },
+        },
+    },
+};
+
+// window.onload = function () {
+//     drawCanvas();
+//     // const ctx01 = document.getElementById("graph01");
+//     // new Chart(ctx01, config);
+
+//     // const ctx02 = document.getElementById("graph02");
+//     // window.mybar01 = new Chart(ctx02, config);
+// };
+
+function drawCanvas() {
+    const ctx01 = document.getElementById("allCancers");
+    new Chart(ctx01, config);
+
+    const ctx02 = document.getElementById("allChronics");
+    new Chart(ctx02, config);
+}
---
+++ pdfMaker/src/js/HTMLWriter.js
@@ -0,0 +1,188 @@
+cancers = ["간암", "위암"];
+
+var percent = 50;
+function get_percent() {
+    return percent;
+}
+
+function writeHTMLs() {
+    //get cancerList
+    for (const cancer of cancers) {
+        writeHtml(cancer);
+    }
+}
+
+function writeHtml(cancer) {
+    document.write("<body>");
+    document.write('<div class="page">');
+    document.write("<header>");
+    document.write('<div class="base">');
+    document.write('<div class="inner">');
+    document.write('<div id="headline">OK AI CHECK</div>');
+    document.write("</div>");
+    document.write("</div>");
+    document.write("</header>");
+    document.write("<main>");
+    document.write('<div class="inner">');
+    document.write('<div id="title">');
+    document.write("<h1>" + cancer + " 위험도 분석</h1>");
+    document.write("</div>");
+    document.write('<section class="indexBlock">');
+    document.write('<div id="warning">');
+    document.write('<div id="percent">');
+    document.write('<p id="percentInfo">AI예측, 5년 내 발병 위험도</p>');
+    document.write('<div id="percentValue">');
+    document.write(get_percent() + "%");
+    document.write("</div>");
+    document.write("</div>");
+    document.write('<a class="disableClick" href="src/">');
+    document.write('<img id="warningImg" src="" />');
+    document.getElementById("warningImg").src = logo_info();
+    document.write("</a>");
+    document.write("</div>");
+    document.write("<hr />");
+    document.write('<p id="warningInfo">');
+    document.write("* 발병 위험도는 1%~99% 의 구간을 가지며 발병율이 높을");
+    document.write("수록 위험도가 높다는 의미입니다. <br />");
+    document.write("정상 1~20%, 관심 21~40%, 주의 41~60%, 경계 61~80%, 위험");
+    document.write("81~99% 로 구분됩니다");
+    document.write("</p>");
+    document.write("<hr />");
+    document.write("</section>");
+    document.write("<h2>분석 지표</h2>");
+    document.write("        ");
+    document.write('<div class="indexBlock">');
+    document.write(writeHTMLGraphs(cancer));
+    // document.write("<!-- 1번 그래프 -->");
+    // document.write('<div class="graph_wrapper">');
+    // document.write('<div class="graph_name">- 저밀도지단백콜레스테롤</div>');
+    // document.write('<div class="indexGraph">');
+    // document.write('<canvas id="index01"></canvas>');
+    // document.write("</div>");
+    // document.write("</div>");
+    // document.write("        ");
+    // document.write("<!-- 2번 그래프 -->");
+    // document.write('<div class="graph_wrapper">');
+    // document.write('<div class="graph_name">- 총콜레스테롤</div>');
+    // document.write('<div class="indexGraph">');
+    // document.write('<canvas id="index02"></canvas>');
+    // document.write("</div>");
+    // document.write("</div>");
+    // document.write("        ");
+    // document.write("<!-- 3번 그래프 -->");
+    // document.write('<div class="graph_wrapper">');
+    // document.write('<div class="graph_name">- 비만도</div>');
+    // document.write('<div class="indexGraph">');
+    // document.write('<canvas id="canvas11"></canvas>');
+    // document.write("</div>");
+    // document.write("</div>");
+    // document.write("        ");
+    // document.write("<!-- 4번 그래프 -->");
+    // document.write('<div class="graph_wrapper">');
+    // document.write('<div class="graph_name">- 고밀도지단백콜레스테롤</div>');
+    // document.write('<div class="indexGraph">');
+    // document.write('<canvas id="canvas11"></canvas>');
+    // document.write("</div>");
+    // document.write("</div>");
+    // document.write("        ");
+    // document.write("<!-- 5번 그래프 -->");
+    // document.write('<div class="graph_wrapper">');
+    // document.write('<div class="graph_name">- 알카라인 포스타파제</div>');
+    // document.write('<div class="indexGraph">');
+    // document.write('<canvas id="canvas11"></canvas>');
+    // document.write("</div>");
+    // document.write("</div>");
+    // document.write("        ");
+    // document.write("<!-- 6번 그래프 -->");
+    // document.write('<div class="graph_wrapper">');
+    // document.write('<div class="graph_name">- 중성지방</div>');
+    // document.write('<div class="indexGraph">');
+    // document.write('<canvas id="canvas11"></canvas>');
+    // document.write("</div>");
+    // document.write("</div>");
+    document.write("</div>");
+    // document.write(getCanvas());
+    document.write(drawIndexGraph());
+    document.write("        ");
+    document.write("<h2>위험도를 낮추기 위한 생활습관 가이드</h2>");
+    document.write('<div class="indexBlock">');
+    document.write('<p id="habitGuidance">');
+    document.write("- 고른 식사와 규칙적 운동이 중요합니다.<br />");
+    document.write("- 지나친 음주는 삼가합니다.<br />");
+    document.write("- 흡연을 하지 않습니다.<br />");
+    document.write("- 정기적인 건강검진을 통해 간 기능을 체크합니다.<br />");
+    document.write("- B형간염 바이러스에 대한 항체가 없다면 B형간염 백신을");
+    document.write("맞습니다.<br />");
+    document.write("- 우상복부 통증, 체중감소, 피로감 등이 발견되면 의사와");
+    document.write("상의합니다.<br />");
+    document.write("- 만성 간질환의 경우 정기적인 초음파검사와 피검사를");
+    document.write("받습니다.<br />");
+    document.write("</p>");
+    document.write("    ");
+    document.write("</div>");
+    document.write("</main>");
+    document.write("    ");
+    document.write("<footer>");
+    document.write('<div class="base">');
+    document.write('<div class="inner">');
+    document.write('<div id="footline">');
+    document.write('<p class="desc">');
+    document.write("서울대 공식 자회사 SNUAiLab과 INFINITYCARE 공동");
+    document.write("연구 개발&nbsp");
+    document.write("</p>");
+    document.write("    ");
+    document.write('<a class="disableClick" href="src/">');
+    document.write('<img id="logo" src="img/infinity_logo.png" />');
+    document.write("</a>");
+    document.write("    ");
+    document.write('<div id="logoDesc">');
+    document.write("인피니티케어<br />R&D 연구센터");
+    document.write("</div>");
+    document.write("</div>");
+    document.write("</div>");
+    document.write("</div>");
+    document.write("</footer>");
+    document.write("</div>");
+    document.write("<!-- </div> -->");
+    document.write("</body>");
+    document.write("</html>");
+}
+
+function writeHTMLGraphs(cancer) {
+    const idxes = getAnalysisIndexes(cancer);
+    for (const [index, element] of idxes.entries()) {
+        document.write('<div class="graphWrapper">');
+        document.write('<div class="indexName">- ' + element + "</div>");
+        document.write('<div class="indexGraph">');
+        document.write(
+            '<canvas id="' +
+                cancer +
+                index +
+                '" style="width: 11cm; height:1.25cm;""></canvas>'
+            // '" width= "11cm" height = "1.25cm" ></canvas>'
+        );
+        document.write("</div>");
+        document.write("<div>" + 55.0 + "</div>");
+        document.write("</div>");
+    }
+}
+
+function getAnalysisIndexes(cancer) {
+    return ["저밀도지단백콜레스테롤", "총콜레스테롤", "비만도"];
+}
+
+function logo_info() {
+    if (percent >= 1 && percent <= 20) {
+        return "img/정상.jpg";
+    } else if (percent > 20 && percent <= 40) {
+        return "img/관심.jpg";
+    } else if (percent > 40 && percent <= 60) {
+        return "img/주의.jpg";
+    } else if (percent > 60 && percent <= 80) {
+        return "img/경계.jpg";
+    } else if (percent > 80 && percent <= 99) {
+        return "img/위험.jpg";
+    } else {
+        console.error("Invalid percent");
+    }
+}
